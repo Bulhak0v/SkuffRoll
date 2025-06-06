5fe7e12227240a02fc8d04b48e814f2b552a86e6
Mykyta <20knight06@gmail.com>
2025-06-06 22:16:05 +0300

    Added apk downloading

--- WebClient/WebClient/wwwroot/src/components/HomepageFooter.js
+++ WebClient/WebClient/wwwroot/src/components/HomepageFooter.js
@@ -2,12 +2,21 @@ import React from 'react';
 import androidIcon from '../assets/images/homepage/android-icon.png';
 import skuffLogo from '../assets/images/homepage/skuff-logo-round.png';
 
+const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/skuffroll.apk';
+    link.setAttribute('download', 'skuffroll.apk');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
 const HomepageFooter = () => {
   return (
     <footer className="app-footer">
-      <button className="footer-android-btn" onClick={() => alert('Android version clicked!')} title="Mobile Version">
-        <img src={androidIcon} alt="Android Version" />
-      </button>
+          <button className="footer-android-btn" onClick={handleDownload} title="Mobile Version">
+              <img src={androidIcon} alt="Android Version" />
+          </button>
       <div className="ukraine-flag">
         <div className="ukraine-flag-blue"></div>
         <div className="ukraine-flag-yellow"></div>
