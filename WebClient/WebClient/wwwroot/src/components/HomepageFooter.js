5ddc8124517bd1446dc984ea76b2da57c1910ed5
Mykyta <20knight06@gmail.com>
2025-06-06 22:32:19 +0300

    Edited apk downloading

--- WebClient/WebClient/wwwroot/src/components/HomepageFooter.js
+++ WebClient/WebClient/wwwroot/src/components/HomepageFooter.js
@@ -2,30 +2,32 @@ import React from 'react';
 import androidIcon from '../assets/images/homepage/android-icon.png';
 import skuffLogo from '../assets/images/homepage/skuff-logo-round.png';
 
-const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = '/skuffroll.apk';
-    link.setAttribute('download', 'skuffroll.apk');
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-};
-
 const HomepageFooter = () => {
-  return (
-    <footer className="app-footer">
-          <button className="footer-android-btn" onClick={handleDownload} title="Mobile Version">
-              <img src={androidIcon} alt="Android Version" />
-          </button>
-      <div className="ukraine-flag">
-        <div className="ukraine-flag-blue"></div>
-        <div className="ukraine-flag-yellow"></div>
-      </div>
-      <div className="footer-logo">
-        <img src={skuffLogo} alt="SkuffRoll Logo" />
-      </div>
-    </footer>
-  );
+    const handleDownload = () => {
+        window.open(
+            'https://drive.google.com/uc?export=download&id=17Mz8BNffcgrzCgIaeirIvg_dA5EG4S4A',
+            '_blank'
+        );
+    };
+
+    return (
+        <footer className="app-footer">
+            <button
+                className="footer-android-btn"
+                onClick={handleDownload}
+                title="Mobile Version"
+            >
+                <img src={androidIcon} alt="Android Version" />
+            </button>
+            <div className="ukraine-flag">
+                <div className="ukraine-flag-blue"></div>
+                <div className="ukraine-flag-yellow"></div>
+            </div>
+            <div className="footer-logo">
+                <img src={skuffLogo} alt="SkuffRoll Logo" />
+            </div>
+        </footer>
+    );
 };
 
 export default HomepageFooter;
